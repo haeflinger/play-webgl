ecce5f76ddcd73aba87a3f459932858c15354823
Kurt Haeflinger <khaeflinger@gmail.com>
2016-08-26 09:13:33 -0500

    Passing Data to WebGL Shaders

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var shaderProgram;
 
 initGL();
 createShaders();
+createVerticies();
 draw();
 
 function initGL(){
@@ -13,10 +14,13 @@ function initGL(){
 }
 
 function createShaders() {
+
   var vs = "";
+  vs += "attribute vec4 coords;";
+  vs += "attribute float pointSize;";
   vs += "void main(void) {";
-  vs += "  gl_Position = vec4(0.5, 0.5, 0.0, 1.0);";
-  vs += "  gl_PointSize = 100.0;";
+  vs += "  gl_Position = coords;";
+  vs += "  gl_PointSize = pointSize;";
   vs += "}";
 
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -24,8 +28,10 @@ function createShaders() {
   gl.compileShader(vertexShader);
 
   var fs = "";
+  fs += "precision mediump float;";
+  fs += "uniform vec4 color;";
   fs += "void main(void) {";
-  fs += "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);";
+  fs += "  gl_FragColor = color;";
   fs += "}";
 
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -37,6 +43,17 @@ function createShaders() {
   gl.attachShader(shaderProgram, fragmentShader);
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
+}
+
+function createVerticies() {
+  var coords = gl.getAttribLocation(shaderProgram, "coords");
+  gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+
+  var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
+  gl.vertexAttrib1f(pointSize, 100);
+
+  var color = gl.getUniformLocation(shaderProgram, "color");
+  gl.uniform4f(color, 1, 0, 1, 1);
 }
 
 function draw() {
