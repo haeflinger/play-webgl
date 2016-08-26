d510b603b3d39dc4e75e79fa3fb23b1f0e2c6e1f
Kurt Haeflinger <khaeflinger@gmail.com>
2016-08-26 13:39:53 -0500

    User Interaction in WebGL

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var gl,
     shaderProgram,
-    verticies,
-    vertextCount = 5000;
+    vertices,
+    vertextCount = 5000,
+    mouseX = 0,
+    mouseY = 0;
+
+canvas.addEventListener("mousemove", function(event) {
+  mouseX = map(event.clientX, 0, canvas.width, -1, 1);
+  mouseY = map(event.clientY, 0, canvas.height, 1, -1);
+});
+
+function map(value, minSrc, maxSrc, minDst, maxDst) {
+  return (value - minSrc) / (maxSrc - minSrc) * (maxDst - minDst) + minDst;
+}
 
 initGL();
 createShaders();
@@ -28,16 +39,16 @@ function createShaders() {
 }
 
 function createVerticies() {
-  verticies = [];
+  vertices = [];
 
   for(var i = 0; i < vertextCount; i++){
-    verticies.push(Math.random() * 2 - 1);
-    verticies.push(Math.random() * 2 - 1);
+    vertices.push(Math.random() * 2 - 1);
+    vertices.push(Math.random() * 2 - 1);
   }
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 
   var coords = gl.getAttribLocation(shaderProgram, "coords");
   //gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
@@ -95,11 +106,20 @@ function getShader(gl, id) {
 
 function draw() {
   for(var i = 0; i < vertextCount * 2; i += 2){
-    verticies[i] += Math.random() * 0.01 - 0.005;
-    verticies[i + 1] += Math.random() * 0.01 - 0.005;
+    var dx = vertices[i] - mouseX,
+        dy = vertices[i + 1] - mouseY,
+        dist = Math.sqrt(dx * dx + dy * dy);
+        if(dist < 0.2) {
+          vertices[i] = mouseX + dx / dist * 0.2;
+          vertices[i + 1] = mouseY + dy / dist * 0.2;
+        } else {
+          vertices[i] += Math.random() * 0.01 - 0.005;
+          vertices[i + 1] += Math.random() * 0.01 - 0.005;
+        }
   }
-  gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(verticies));
+  gl.bufferSubData(gl.ARRAY_BUFFER, 0, new Float32Array(vertices));
   gl.clear(gl.COLOR_BUFFER_BIT);
   gl.drawArrays(gl.POINTS, 0, vertextCount);
+
   requestAnimationFrame(draw);
 }
