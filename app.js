d0ed35bf26335e1afc178d8d19f789056620db6b
Kurt Haeflinger <khaeflinger@gmail.com>
2016-08-26 09:22:20 -0500

    WebGL Vertex Buffers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var gl;
-var shaderProgram;
+var gl,
+    shaderProgram,
+    verticies;
 
 initGL();
 createShaders();
@@ -46,17 +47,30 @@ function createShaders() {
 }
 
 function createVerticies() {
+  verticies = [
+    -0.9, -0.9, 0.0,
+     0.9, -0.9, 0.0,
+     0.0,  0.9, 0.0
+  ];
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verticies), gl.STATIC_DRAW);
+
   var coords = gl.getAttribLocation(shaderProgram, "coords");
-  gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+  //gl.vertexAttrib3f(coords, 0.5, 0.5, 0);
+  gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(coords);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
   var pointSize = gl.getAttribLocation(shaderProgram, "pointSize");
-  gl.vertexAttrib1f(pointSize, 100);
+  gl.vertexAttrib1f(pointSize, 20);
 
   var color = gl.getUniformLocation(shaderProgram, "color");
-  gl.uniform4f(color, 1, 0, 1, 1);
+  gl.uniform4f(color, 0, 0, 0, 1);
 }
 
 function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
