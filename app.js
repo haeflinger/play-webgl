23298b35a2c71dad8beec02bde5ba43d880d2ad7
Kurt Haeflinger <khaeflinger@gmail.com>
2016-08-26 09:02:49 -0500

    Introduction to WebGL Shaders

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var gl;
+var shaderProgram;
 
 initGL();
 createShaders();
@@ -14,7 +15,8 @@ function initGL(){
 function createShaders() {
   var vs = "";
   vs += "void main(void) {";
-  vs += "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);";
+  vs += "  gl_Position = vec4(0.5, 0.5, 0.0, 1.0);";
+  vs += "  gl_PointSize = 100.0;";
   vs += "}";
 
   var vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -23,15 +25,21 @@ function createShaders() {
 
   var fs = "";
   fs += "void main(void) {";
-  fs += "  gl+FragColor = vec4(0.0, 0.0, 0.0, 1.0);";
+  fs += "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);";
   fs += "}";
 
   var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShader, fs);
   gl.compileShader(fragmentShader);
+
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
 }
 
 function draw() {
   gl.clear(gl.COLOR_BUFFER_BIT);
-
+  gl.drawArrays(gl.POINTS, 0, 1);
 }
