d45a5aecfd9040aa0b86abc5fe7e69fc46fb7ed2
Kurt Haeflinger <khaeflinger@gmail.com>
2016-08-26 14:08:39 -0500

    Using a Matrix Library for WebGL

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var gl,
     shaderProgram,
     verticies,
-    angle = 0;
+    matrix = mat4.create(),
+    vertexCount = 30;
 
 initGL();
 createShaders();
@@ -28,11 +29,13 @@ function createShaders() {
 }
 
 function createVerticies() {
-  verticies = [
-    -0.9, -0.9, 0.0,
-     0.9, -0.9, 0.0,
-     0.0,  0.9, 0.0
-    ];
+  verticies = [];
+
+  for(var i = 0; i < vertexCount; i++) {
+    verticies.push(Math.random() * 2 -1);
+    verticies.push(Math.random() * 2 -1);
+    verticies.push(Math.random() * 2 -1);
+  }
 
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -53,34 +56,17 @@ function createVerticies() {
 
 
 function draw() {
-  rotateY(angle += 0.01);
+  mat4.rotateZ(matrix, matrix, -0.007);
+  mat4.rotateY(matrix, matrix, 0.014);
+  mat4.rotateZ(matrix, matrix, 0.01);
+  var transformMatrix = gl.getUniformLocation(shaderProgram, "transformMatrix");
+  gl.uniformMatrix4fv(transformMatrix, false, matrix);
+
   gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
   requestAnimationFrame(draw);
 }
 
-function rotateZ(angle) {
-  var cos = Math.cos(angle),
-      sin = Math.sin(angle),
-      matrix = new Float32Array([ cos, sin,  0,  0,
-                                 -sin, cos,  0,  0,
-                                    0,   0,  1,  0,
-                                    0,   0,  0,  1]);
-  var transformMatrix = gl.getUniformLocation(shaderProgram, "transformMatrix");
-  gl.uniformMatrix4fv(transformMatrix, false, matrix);
-}
-
-
-function rotateY(angle) {
-  var cos = Math.cos(angle),
-      sin = Math.sin(angle),
-      matrix = new Float32Array([ cos,   0, sin,  0,
-                                    0,   1,   0,  0,
-                                 -sin,   0, cos,  0,
-                                    0,   0,   0,  1]);
-  var transformMatrix = gl.getUniformLocation(shaderProgram, "transformMatrix");
-  gl.uniformMatrix4fv(transformMatrix, false, matrix);
-}
 
 
 
